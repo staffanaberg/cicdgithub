ba19d958107e97fd3892670ef7228e9fde8838e5
Staffan Åberg <staffan.aaberg@gmail.com>
2024-10-15 15:47:55 +0200

    First commit

---
+++ src/main/java/user/User.java
@@ -0,0 +1,18 @@
+package user;
+
+public class User {
+
+    //private datatype attributeName
+    //private int number;
+    private String username;
+    private String password;
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    
+}
---
+++ src/test/java/testuser/TestUser.java
@@ -0,0 +1,33 @@
+package testuser;
+
+import org.junit.jupiter.api.Test;
+import user.User;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestUser {
+
+    @Test
+    public void testUsername() {
+        //Skapa ett objekt av klassen User
+        User myUser = new User("henryviii", "password");
+
+        String myUsername = myUser.getUsername();
+
+        assertEquals("henryviii", myUsername);
+
+    }
+
+    @Test
+    public void testPassword() {
+        //Skapa ett objekt av klassen User
+        User myUser = new User("henryviii", "password");
+
+        String myUsername = myUser.getUsername();
+
+        assertEquals("henryviii", myUsername);
+
+    }
+
+
+}
